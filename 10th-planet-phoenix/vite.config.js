6521737e918efe1941a37e0f1d8c43d39abcbaf8
caleblopez96 <caleblopez96@gmail.com>
2025-03-16 16:43:18 -0700

    attempt to fix

--- 10th-planet-phoenix/vite.config.js
+++ 10th-planet-phoenix/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/10th-planet-phoenix",
+  base: "/10th-planet-phoenix/10th-planet-phoenix",
 });
