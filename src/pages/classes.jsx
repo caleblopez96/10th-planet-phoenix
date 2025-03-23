a2821bdccc5d877363c0e49034faab684f18a0cd
caleblopez96 <caleblopez96@gmail.com>
2025-03-22 19:41:53 -0700

    created a resuable hero2 to be used across pages that arent home

--- src/pages/classes.jsx
+++ src/pages/classes.jsx
@@ -1,6 +1,11 @@
 import ClassSchedule from "../Components/ClassSchedule/ClassSchedule";
 import ClassesSection from "../Components/ClassesSection/ClassesSection";
 import FAQ from "../Components/FAQ/FAQ";
+import Hero2 from "../Components/Hero/Hero2";
+
+// image imports
+import classes1 from "../assets/classes/classes1.jpg";
+import classes2 from "../assets/classes/classes2.jpg";
 
 const classesFAQs = [
   { question: "", answer: "" },
@@ -13,6 +18,12 @@ const classesFAQs = [
 const Classes = () => {
   return (
     <>
+      <Hero2
+        imageOne={classes1}
+        imageTwo={classes2}
+        content={"Classes"}
+        height={"75vh"}
+      />
       <ClassSchedule />
       <ClassesSection />
       <FAQ faqs={classesFAQs} />
--- src/pages/instructors.jsx
+++ src/pages/instructors.jsx
@@ -1,13 +1,19 @@
+import eddie from "../Components/Information/eddie-bravo.jpg";
+import johnKeith from "../assets/HERO SM/IMG_20210703_121346.jpg";
+
 import EliteInstructors from "../Components/EliteInstructors/EliteInstructors";
-
 import InstructorBio from "../Components/EliteInstructors/InstructorBio";
-
 import Hero2 from "../Components/Hero/Hero2";
 
 const Instructors = () => {
   return (
     <>
-      <Hero2 content={"Instructors"} />
+      <Hero2
+        imageOne={eddie}
+        imageTwo={johnKeith}
+        content={"Instructors"}
+        height="75vh"
+      />
       <EliteInstructors />
       <InstructorBio />
     </>
