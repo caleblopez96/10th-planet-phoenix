ab58757164a311b6d4257061ce5ba52d2d2a0c2d
caleblopez96 <caleblopez96@gmail.com>
2025-03-28 12:36:47 -0700

    Hero: Changed button routing components from NavLinks to improve accessibility

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ import mobile4 from "../../assets/HERO SM/IMG_20210703_121346.jpg";
 // UI Components
 import ButtonDark from "../Buttons/ButtonDark";
 import ButtonLight from "../Buttons/ButtonLight";
-import { NavLink, Route } from "react-router-dom";
+import { Link, NavLink, Route } from "react-router-dom";
 
 const desktopImages = [desktop1, desktop2, desktop3, desktop4];
 const mobileImages = [mobile1, mobile2, mobile3, mobile4];
@@ -61,14 +61,13 @@ const Hero = () => {
         </p>
 
         <div className="flex justify-center gap-4 mt-6 sm:mt-4">
-          {/* idk if this is best practice. research into it */}
-          {/* the other way im thinking is add it to the component, but right now im rendering the button dynamically based on the content prop*/}
-          <NavLink to={"/contact"}>
+          <Link to={"/contact"}>
             <ButtonLight content={"Get Started"} />
-          </NavLink>
-          <NavLink to={"/about"}>
+          </Link>
+
+          <Link to={"/about"}>
             <ButtonDark content={"Learn More"} />
-          </NavLink>
+          </Link>
         </div>
       </div>
     </section>
