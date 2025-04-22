d8f9e4afc460e56d3868677743ee7981ab1e213a
caleblopez96 <caleblopez96@gmail.com>
2025-04-22 16:22:28 -0700

    minor style adjustments and cahgnes to markup to make it more semantic

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,10 +14,10 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-10 mb-8">
           <div className="flex flex-col gap-4">
             <NavLink to="/" className="flex-col">
-              <h3 className="font-bold text-lg">10th Planet Phoenix</h3>
+              <h3 className="font-bold text-2xl">10th Planet Phoenix</h3>
             </NavLink>
 
-            <p className="">
+            <p className="text-sm">
               Training Brazilian Jiu-Jitsu athletes in the Phoenix area since
               2010.
             </p>
@@ -25,7 +25,7 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col gap-4">
-            <h3 className="font-bold text-lg">Quick Links</h3>
+            <h3 className="font-bold text-2xl">Quick Links</h3>
             <nav>
               <ul className="space-y-2">
                 <li>
@@ -59,7 +59,7 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col gap-4">
-            <h3 className="font-bold text-lg">Contact Us</h3>
+            <h3 className="font-bold text-2xl">Contact Us</h3>
             <ul className="space-y-2">
               <li className="flex items-start">
                 <span className="mr-2">
@@ -120,20 +120,18 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col gap-4">
-            <h3 className="font-bold text-lg">Hours</h3>
-            <ul className="space-y-2">
-              <li className="flex items-start">
-                <div>
-                  <p>Mon: 10-11 AM, 5:00-8:30 PM</p>
-                  <p>Tue: 5:30-7 AM, 5-8:30 PM</p>
-                  <p>Wed: 5:30-7 AM, 6-8:30 PM</p>
-                  <p>Thur: 5:30-7 AM, 6-8 PM</p>
-                  <p>Fri: CLOSED</p>
-                  <p>Sat: 10AM - 1 PM</p>
-                  <p>Sun: CLOSED</p>
-                </div>
-              </li>
-            </ul>
+            <h3 className="font-bold text-2xl">Hours</h3>
+            <div className="flex">
+              <ul className="space-y-2">
+                <li>Mon: 10-11 AM, 5:00-8:30 PM</li>
+                <li>Tue: 5:30-7 AM, 5-8:30 PM</li>
+                <li>Wed: 5:30-7 AM, 6-8:30 PM</li>
+                <li>Thur: 5:30-7 AM, 6-8 PM</li>
+                <li>Fri: CLOSED</li>
+                <li>Sat: 10AM - 1 PM</li>
+                <li>Sun: CLOSED</li>
+              </ul>
+            </div>
           </div>
         </div>
 
@@ -149,13 +147,14 @@ const Footer = () => {
               width="40"
               height="40"
               viewBox="0 0 24 24"
+              className="hover:fill-red-600"
             >
               <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
             </svg>
           </a>
           <a
             href="https://www.facebook.com/10thplanetphx/"
-            className="text-gray-600 hover:text-red-600 transition-colors text-2xl"
+            className="text-gray-600 transition-colors text-2xl"
             aria-label="Facebook"
             target="_blank"
             rel="noopener noreferrer"
@@ -165,6 +164,7 @@ const Footer = () => {
               width="40"
               height="40"
               viewBox="0 0 24 24"
+              className="hover:fill-red-600"
             >
               <path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h7.621v-6.961h-2.343v-2.725h2.343V9.309 c0-2.324,1.421-3.591,3.495-3.591c0.699-0.002,1.397,0.034,2.092,0.105v2.43h-1.428c-1.13,0-1.35,0.534-1.35,1.322v1.735h2.7 l-0.351,2.725h-2.365V21H19c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z"></path>
             </svg>
@@ -180,6 +180,7 @@ const Footer = () => {
               width="40"
               height="40"
               viewBox="0 0 24 24"
+              className="hover:fill-red-600"
             >
               <path d="M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z"></path>
             </svg>
